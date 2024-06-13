bec3ad69dbad26c260ed9a4201fb6ab8836898cd
p-tecza <pawel.tecza.pt@gmail.com>
2024-06-13 03:12:30 +0200

    refactor2

--- diff/Graph.jl
+++ diff/Graph.jl
@@ -14,8 +14,7 @@ mutable struct Variable <: GraphNode
     gradient::Any
     name::String
     batch_gradient::Any
-    recurrent_procedure::Bool
-    Variable(output; name="?") = new(output, nothing, name, nothing, true)
+    Variable(output; name="?") = new(output, nothing, name, nothing)
 end
 
 mutable struct BroadcastedOperator{F} <: Operator
@@ -40,7 +39,6 @@ function visit(node::Operator, visited, order)
     if node ∉ visited
         push!(visited, node)
         for input in node.inputs
-            # if isa(node, BroadcastedOperator{typeof(recurrence)}) && isa(input, BroadcastedOperator{typeof(tanh)}) 
             visit(input, visited, order)
         end
         push!(order, node)
@@ -56,7 +54,6 @@ function topological_sort(head::GraphNode)
 end
 
 import Base: show, summary
-# show(io::IO, x::ScalarOperator{F}) where {F} = print(io, "op ", x.name, "(", F, ")");
 show(io::IO, x::BroadcastedOperator{F}) where {F} = print(io, "op.", "BR_OP", "(", F, ")");
 show(io::IO, x::Constant) = print(io, "const ", x.output)
 show(io::IO, x::Variable) = begin
@@ -71,72 +68,25 @@ end
 include("Operators.jl")
 
 function build_graph(input1, input2, input3, input4, start_hidden, output, i_weights, h_weights, out_weights, biases)
-
-    known_output = output
-    # i_weights = Variable(i_weights, name="ZMIENNE_WEJSCIE")
-    # h_weights = Variable(h_weights, name="ZMIENNE_UKRYTE")
     h = Constant(start_hidden)
-    recurrence_started = false
-    # @show size(full_input)
-    res1 = initial_recurrence(i_weights, h_weights, input1, h, biases) |> tanh # to tanh raczej nie tu
+    res1 = initial_recurrence(i_weights, h_weights, input1, h, biases) |> tanh
     res2 = recurrence(i_weights, h_weights, input2, res1, biases) |> tanh
     res3 = recurrence(i_weights, h_weights, input3, res2, biases) |> tanh
     res = recurrence(i_weights, h_weights, input4, res3, biases) |> tanh
     l_dense = dense(res, out_weights) |> identity_transpose
-    e = cross_entropy_loss(l_dense, known_output)
-
+    e = cross_entropy_loss(l_dense, output)
     return topological_sort(e)
 end
 
 function update_weights!(graph::Vector, lr::Float64, batch_size::Int64)
     for node in graph
         if isa(node, Variable) && hasproperty(node, :batch_gradient)
-            # if length(node.batch_gradient) == 1
-            #     node.batch_gradient /= batch_size
-            # else
-
-            # end
-
-            # println(node.name,".output:")
-
             node.batch_gradient ./= batch_size
-
-            # if length(node.output) == 1
-            #     node.output -= lr * node.batch_gradient 
-            # else
-            #     node.output .-= lr * node.batch_gradient 
-            # end
-            # @show size(node.output)
-            # @show size(node.batch_gradient)
-            # @show node.name
-            # println("UPDATE WEIGHTS WAGI PRZED: ",node.output)
             node.output .-= lr * node.batch_gradient
-            # weights_to_rescale = node.output
-            # limited_weight = min.(weights_to_rescale, 1)
-            # limited_weight = max.(weights_to_rescale, -1)
-            # node.output = weights_to_rescale
-            # node.output = 2 .* ((node.output .- minimum(weights_to_rescale)) ./ (maximum(weights_to_rescale) - minimum(weights_to_rescale))) .- 1
-            
-            if isa(node.output, Matrix)
-
-
-                # println("maxweight: ", maximum(node.output))
-                # println("minweight: ", minimum(node.output))
-
-                # println("WAGA ",node.name,":",node.output[1:10])
-            elseif node.name == "BIASES"
-                # println("BIASES: ",node.output[1:10])
-            end
-
-
-            # println("UPDATE WEIGHTS WAGI PO: ",node.output)
-            node.batch_gradient = nothing
-            # fill(node.batch_gradient, nothing)
+            fill(node.batch_gradient, 0)
         end
     end
 end
-
-# @show 5.0
 
 function forward!(order::Vector)
     for node in order
@@ -147,149 +97,61 @@ function forward!(order::Vector)
 end
 
 reset!(node::Constant) = nothing
-reset!(node::Variable) = let
-    node.gradient = nothing
-    node.recurrent_procedure = true
-end
+reset!(node::Variable) =
+    let
+        node.gradient = nothing
+    end
 reset!(node::Operator) = node.gradient = nothing
 
 compute!(node::Constant) = nothing
 compute!(node::Variable) = nothing
 compute!(node::Operator) = node.output = forward(node, [input.output for input in node.inputs]...)
 
-update!(node::Constant, gradient, final_input_gradient::String) =
+update!(node::Constant, gradient, pick_gradient::Bool) =
     let
-        # println("UPDATE! = NOTHING??????")
         nothing
     end
-update!(node::GraphNode, gradient, final_input_gradient::String) =
+update!(node::GraphNode, gradient, pick_gradient::Bool) =
     let
-        # @show gradient
-        # for i in eachindex(gradient)
-        #     if gradient[i] < -5.
-        #         gradient[i] = -5.
-        #     elseif gradient[i] > 5.
-        #         gradient[i] = 5.
-        #     end
-        # end
-        
-        # println("ROZMIAR GRADIENTU: ", size(gradient))
-        # println("TYP GRADIENTU: ", typeof(gradient))
-        # if typeof(gradient) == Matrix{Float64}
-        #     # @show gradient[1:7,1:7]
-        #     limited_grad = min.(gradient, 5)
-        #     limited_grad = max.(limited_grad, -5)
-        #     # @show limited_grad[1:7,1:7]
-        #     gradient = limited_grad
-        # end
-
         node.gradient = gradient
-
-
-        # if isa(node,Variable) && node.name == "BIASES"
-        #     @show node.gradient
-        # end
-
         if typeof(node) == Variable
-            if final_input_gradient == "HIDDEN" && (node.name == "ZMIENNE_WEJSCIE" || node.name == "BIASES")
+            if pick_gradient && node.name != "ZMIENNE_OUT"
                 if isnothing(node.batch_gradient)
                     node.batch_gradient = gradient
                 else
-                    node.batch_gradient .+= gradient
-                end
-            elseif final_input_gradient == "HIDDEN" && node.name == "ZMIENNE_UKRYTE"
-                if isnothing(node.batch_gradient)
-                    # println("[B] INICJUJE BATCH GRADIENT W VARZE: " * node.name)
-                    # println("PRZYPISUJE HIDDEN")
-                    node.batch_gradient = gradient
-                else
-                    # println(node.name,":",node)
-                    # println("[B] DODAJE GRADIENT DO BATCH GRADIENT W VARZE: ", node.name)
-                    # println(node.name)
-                    # println(size(gradient))
-                    # println(size(node.batch_gradient))
-                    # println("DODAJE HIDDEN")
                     node.batch_gradient .+= gradient
                 end
             elseif node.name == "ZMIENNE_OUT"
                 if isnothing(node.batch_gradient)
-                    # println("PRZYPISUJE OUT")
-                    # println("[B] INICJUJE BATCH GRADIENT W VARZE: " * node.name)
                     node.batch_gradient = gradient
                 else
-                    # println("[B] DODAJE GRADIENT DO BATCH GRADIENT W VARZE: ", node.name)
-                    # println(node.name)
-                    # println(size(gradient))
-                    # println(size(node.batch_gradient))
-                    # println("DODAJE OUT")
                     node.batch_gradient .+= gradient
                 end
             end
         end
     end
 
-function backward!(node::Constant, final_input_gradient::String) end
-function backward!(node::Variable, final_input_gradient::String) end
+function backward!(node::Constant, pick_gradient::Bool) end
+function backward!(node::Variable, pick_gradient::Bool) end
 function backward!(order::Vector; seed=1.0)
     result = last(order)
     result.gradient = seed
     @assert length(result.output) == 1 "Gradient is defined only for scalar functions"
     for (index, node) in enumerate(reverse(order))
-        # println("PODROZ W TYL: ", index, node)
-        if index == 16 # 16 to index ostatniej pierwszej warstwy recurrence, z niej bierzemy gradient DLA HIDDEN (a dla INPUT i BIASES index 7)
-            # println("INDEX: ",index," NODE: ",node)
-            backward!(node, "")
-        elseif index == 7
-            backward!(node, "HIDDEN") # "INPUT_AND_BIASES"
+        if index == 7 # gradienty z 7. node są kluczowe do nauki
+            backward!(node, true)
         else
-            backward!(node, "")
+            backward!(node, false)
         end
-        if isa(node, BroadcastedOperator) # obcinanie gradientu
-            gradient = node.gradient
-            if typeof(gradient) == Matrix{Float64}
-                # # @show gradient[1:7,1:7]
-                # limited_grad = min.(gradient, 5)
-                # limited_grad = max.(limited_grad, -5)
-                # # @show limited_grad[1:7,1:7]
-                # node.gradient = limited_grad
-            else
-
-            end
-        
-        end
-        
     end
     return nothing
 end
 
-function backward!(node::Operator, final_input_gradient::String)
+function backward!(node::Operator, pick_gradient::Bool)
     inputs = node.inputs
-    # println("INPUTS: ")
-    # for i in inputs
-    #     @show typeof(i)
-    #     @show size(i.output)
-    # end
-
-    # @show size(node.gradient)
-
     gradients = backward(node, [input.output for input in inputs]..., node.gradient)
-    #@show inputs
-    #@show gradients
-    # println("START UPDATE!")
-    # @show inputs
-    # @show gradients
     for (input, gradient) in zip(inputs, gradients)
-
-        # @show size(gradient)
-        # @show size(gradient)
-        # @show input
-        # if length(gradients) > 1 && isa(node, BroadcastedOperator{typeof(recurrence)}) && isa(input, BroadcastedOperator{typeof(tanh)})
-        #     gradient = gradients[2]
-        # elseif length(gradients) > 1 && isa(node, BroadcastedOperator{typeof(recurrence)}) && isa(input, Variable) && input.name == "ZMIENNE_WEJSCIE"
-        #     gradient = gradients[1]
-        # end
-        update!(input, gradient, final_input_gradient)
+        update!(input, gradient, pick_gradient)
     end
-    # println("KONIEC UPDATE!")
     return nothing
 end
--- net/RNN.jl
+++ net/RNN.jl
@@ -1,79 +1,31 @@
-
-function relu(x::Float64)
-    return max(x, zero(x))
-end
-
-function tanh(x)
-    return (exp.(x) - exp.(-x)) / (exp.(x) + exp.(-x)) # 97.65832
-end
-
-function pass!()
-
-end
-
-function rnn_cell(input, hidden_state, weights_i, weights_h)
-    hidden_state = tanh.(weights_i * input .+ weights_h * hidden_state)
-    return hidden_state, hidden_state
-end
-
-
 include("../diff/Graph.jl")
-using Statistics: mean
 using Flux: glorot_uniform
-# x = rand(Float32, input_size) # dummy input data
-
-# y = [1.555,2.777]
-
-
-# @show x
-# @show h
-# @show y
-
 
 input_size = 196
 hidden_output_size = 64
 output_size = 10
 
-Wxh = glorot_uniform(hidden_output_size, input_size) # pojedynczy neuron, 196 wejsc, 64 wyjscia -> wagi dla wejsc
-# @show Wxh
-Whh = glorot_uniform(hidden_output_size, hidden_output_size) # wagi wyjsc (64 wyjscia)
-# @show Whh
+Wxh = glorot_uniform(hidden_output_size, input_size)
+Whh = glorot_uniform(hidden_output_size, hidden_output_size)
 Why = glorot_uniform(output_size, hidden_output_size)
-
 start_hidden = zeros(hidden_output_size)
-# h = zeros(hidden_output_size) # random initial hidden state
-
 biases = zeros(hidden_output_size)
-
-@show size(Why)
-@show typeof(Why)
-# b   = randn(Float32, output_size)
 
 using MLDatasets, Flux
 
-
 PART_DATA_AMOUNT = 60000;
 train_data = MLDatasets.MNIST(split=:train)
-x1dim = reshape(train_data.features, 28 * 28, :) # reshape 28×28 pixels into a vector of pixels
-yhot = Flux.onehotbatch(train_data.targets, 0:9) # make a 10×60000 OneHotMatrix
+x1dim = reshape(train_data.features, 28 * 28, :)
+yhot = Flux.onehotbatch(train_data.targets, 0:9)
 part_of_train_data = x1dim[:, 1:PART_DATA_AMOUNT]
 splitted_train_data = [part_of_train_data[1:196, :], part_of_train_data[197:392, :], part_of_train_data[393:588, :], part_of_train_data[589:end, :]];# 
 y_train_part = yhot[:, 1:PART_DATA_AMOUNT]
-
-# println("SIZE OF TEST DATA: " * string(size(x1dim)))
-
-
-
-
 
 x1_train = Constant(part_of_train_data[1:196, 1])
 x2_train = Constant(part_of_train_data[197:392, 1])
 x3_train = Constant(part_of_train_data[393:588, 1])
 x4_train = Constant(part_of_train_data[589:end, 1])
 y_train = Constant(y_train_part[:, 1])
-@show size(y_train_part[:, 1])
-
-@show part_of_train_data[1:196, 1]
 
 i_weights = Variable(Wxh, name="ZMIENNE_WEJSCIE")
 h_weights = Variable(Whh, name="ZMIENNE_UKRYTE")
@@ -81,24 +33,12 @@ o_weights = Variable(Why, name="ZMIENNE_OUT")
 b = Variable(biases, name="BIASES")
 
 graph = build_graph(x1_train, x2_train, x3_train, x4_train, start_hidden, y_train, i_weights, h_weights, o_weights, b)
-# last_order_output = last(graph).output 
 
-
-for x in graph
-    @show typeof(x)
-end
-
-# println("Przed forward")
-#@show graph
-#@show last_order_output
-
-#TODO jest popsute, siadz do tego na spokojnie...
-
-# IT_NUM = 20
 BATCH_SIZE = 100
 EPOCHS_NUM = 5
 LR = 15e-3
 
+println("Training started...")
 @time for j in 1:EPOCHS_NUM
     global num_of_clasiffications = 0
     global num_of_correct_clasiffications = 0
@@ -106,7 +46,6 @@ LR = 15e-3
         blad_petla = forward!(graph)
         backward!(graph)
         if i % BATCH_SIZE == 0
-            # println("Funkcja starty: ", blad_petla)
             update_weights!(graph, LR, BATCH_SIZE)
         end
         x1_train.output = part_of_train_data[1:196, i]
@@ -136,13 +75,6 @@ x2_test = Constant(part_of_test_data[197:392, 1])
 x3_test = Constant(part_of_test_data[393:588, 1])
 x4_test = Constant(part_of_test_data[589:end, 1])
 y_test = Constant(y_test_part[:, 1])
-
-# hidden_init_state = glorot_uniform(hidden_output_size) # random initial hidden state
-
-println("WAGA i_weights",i_weights,":",i_weights.output[1:10])
-println("WAGA h_weights",h_weights,":",h_weights.output[1:10])
-println("WAGA o_weights",o_weights,":",o_weights.output[1:10])
-println("BIASES POZA: ",b, ":",b.output[1:10])
 
 test_graph = build_graph(x1_test, x2_test, x3_test, x4_test, start_hidden, y_test, i_weights, h_weights, o_weights, b)
 
